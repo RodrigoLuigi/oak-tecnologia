059093c4bed329c79ea072194eae2fe26c232f61
Rodrigo Luigi <digaor@gmail.com>
2024-07-07 09:13:10 -0300

    add: knex query builder, create connection and knexfile config

---
+++ api/knexfile.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import { Knex } from 'knex';
+import { Database } from 'sqlite3';
+
+const config: {[key: string]: Knex.Config} = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db'),
+    },
+    pool: {
+      afterCreate: (conn: Database, cb: (err: Error | null) => void) => {
+        conn.run('PRAGMA foreign_keys = ON', cb);
+      },
+    },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        'src',
+        'database',
+        'knex',
+        'migrations'
+      ),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'seeds'),
+    },
+    useNullAsDefault: true,
+  },
+};
+
+export default config;
