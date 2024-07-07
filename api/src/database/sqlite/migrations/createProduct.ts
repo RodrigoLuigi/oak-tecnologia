44c24683c6eaebac3be6a9e648ac6093dc175a48
Rodrigo Luigi <digaor@gmail.com>
2024-07-07 08:55:07 -0300

    refact: createProduct sql migration

--- api/src/database/sqlite/migrations/createProduct.ts
+++ api/src/database/sqlite/migrations/createProduct.ts
@@ -1,11 +1,11 @@
 const createProduct = `
   CREATE TABLE IF NOT EXISTS product(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    price FLOAT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price REAL NOT NULL CHECK(typeof(price) = 'real' OR typeof(price) = 'integer'),
+    created_at DATETIME DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'localtime')),
+    updated_at DATETIME DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'localtime'))
   )
 `;
 
