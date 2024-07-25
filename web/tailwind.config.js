c54c6f80bf9a9ca6447011f5ead8bd6ce521f907
Rodrigo Luigi <digaor@gmail.com>
2024-07-24 23:18:33 -0300

    create search and product content list

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      height: {
+        'max-content': 'calc(100vh - 350px)',
+      },
       fontFamily: {
         sans: 'Inter',
       },
