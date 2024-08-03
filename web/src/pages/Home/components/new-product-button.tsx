8929f2d7b4f5cedb2cf778b4178a8302caaafcd4
Rodrigo Luigi <digaor@gmail.com>
2024-08-02 21:07:57 -0300

    style: adjust global style and new product button

--- web/src/pages/Home/components/new-product-button.tsx
+++ web/src/pages/Home/components/new-product-button.tsx
@@ -3,7 +3,7 @@ import { Plus } from 'lucide-react'
 export function NewProductButton({ ...rest }) {
   return (
     <button
-      className="grid-in-newProduct bg-green-400 text-green-800 flex items-center justify-center gap-2.5 font-bold transition hover:bg-green-500 hover:text-green-50"
+      className="grid-in-newProduct bg-green-600  flex items-center justify-center gap-2.5 font-bold transition  hover:bg-green-400 hover:text-green-900"
       {...rest}
     >
       <Plus className="size-8" />
