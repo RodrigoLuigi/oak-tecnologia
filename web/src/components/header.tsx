ee220d2fc8d1e6b25bcf6dfbe0048519d2472922
Rodrigo Luigi <digaor@gmail.com>
2024-08-08 18:57:02 -0300

    refact: links from social media

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -5,22 +5,28 @@ export function Header() {
     <header className="flex flex-col justify-end gap-2 items-center bg-customDark grid-in-header p-6 border-b border-solid border-customGreen lg:items-center lg:flex-row lg:p-0 lg:px-24">
       <div className="flex justify-center gap-4 lg:flex-1">
         <a
-          href="#"
+          href="https://www.instagram.com/lrodrigo_luigi/"
           className="size-9 lg:size-12 bg-[#18181B] rounded-md flex items-center justify-center text-[#47B368] hover:text-[#6bfa98]"
+          target="_blank"
+          rel="noreferrer"
         >
           <Instagram className="size-6 lg:size-8" />
         </a>
 
         <a
-          href="#"
+          href="https://www.linkedin.com/in/rodrigo-luigi/"
           className="size-9 lg:size-12 bg-[#18181B] rounded-md flex items-center justify-center text-[#47B368] hover:text-[#6bfa98]"
+          target="_blank"
+          rel="noreferrer"
         >
           <Linkedin className="size-6 lg:size-8" />
         </a>
 
         <a
-          href="#"
+          href="https://github.com/RodrigoLuigi"
           className="size-9 lg:size-12 bg-[#18181B] rounded-md flex items-center justify-center text-[#47B368] hover:text-[#6bfa98]"
+          target="_blank"
+          rel="noreferrer"
         >
           <Github className="size-6 lg:size-8" />
         </a>
